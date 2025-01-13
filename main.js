d72d0562b77cef42aeace786c25fddb210e0a72b
Alejandro Perez Naitoh <alejandropereznaitoh@APN-MacBook.local>
2025-01-13 16:27:40 -0600

    1. Create addTask and deleteTask for Project class. 2. Create addProject, deleteProject and getProjects for manageProjects. 3. Create togglePriority and toggleCompleted for Task class. 4. Create createTask for manageTasks.

--- main.js
+++ main.js
@@ -7,40 +7,62 @@ const manageProjects = (() => {
             this.title = title;
             this.tasks = [];
         }
-        addTask() {
+
+        addTask(task) {
+            this.tasks.push(task);
         }
-        deleteTask() {
+
+        deleteTask(taskID) {
+            this.tasks = this.tasks.filter(task => task.id !== taskID);
         }
+        
     }
 
-    const addProject = () => {};
-    const deleteProject = () => {};
-    const getProjects = () => {};
-    return {};
+    const addProject = (id, title) => {
+        const project = new Project(id, title);
+        projects.push(project);
+    };
+
+    const deleteProject = (id) => {
+        projects = projects.filter(project => project.id !== id);
+    };
+
+    const getProjects = () => projects;
+
+    return { addProject, 
+             deleteProject, 
+             getProjects 
+    };
 })();
 
 
 const manageTasks = (() => {
     class Task {
-        constructor(id, name, description, dueDate, status = 'pending') {
+        constructor(id, name, description, dueDate, isPending = true, priority = false) {
             this.id = id;
             this.name = name;
             this.description = description;
             this.dueDate = dueDate;
-            this.status = status;
+            this.isPending = isPending;
+            this.priority = priority;
         }
 
-        toggleStatus() {
+        togglePriority(){
+            this.priority = this.priority ? false : true;
         }
-        markComplete() {
+
+        toggleComplete() {
+            this.isPending = this.isPending ? false : true;
+
         }
     }
 
-    const createTask = () => {
+    const createTask = (id, name, description, dueDate, isPending, priority) => {
+        return new Task (id, name, description, dueDate, isPending, priority)
     };
-    return {};
-})();
 
+    return { createTask };
+})();
 
 const manageDOM = () => {
     const toggleStatus = () => {}
@@ -49,6 +71,14 @@ const manageDOM = () => {
     const displayTasks = () => {}
     const toggleProjectVisibility = () => {}
     const filterTasks = () => {}
+
+    return { toggleStatus, 
+             displayAddProject, 
+             displayAddTask, 
+             displayTasks, 
+             toggleProjectVisibility,
+             filterTasks
+            }
 }
 
 
