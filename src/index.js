f9df883bd5df09853cb33c864c85241bf736abeb
Alejandro Perez Naitoh <alejandropereznaitoh@APN-MacBook.local>
2025-02-17 13:29:25 -0600

    Initialize project. Create HTML structure and style. Create projects and tasks modules. Create Projects Class, addTask, deleteTask, addProject, deleteProject, getProjects and test all. Createclass Task, togglePriority, toggleComplete, createNew task and test.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,16 @@
+// src/index.js
+import './styles.css';  
 import manageProjects from './projects.js';
 import manageTasks from './tasks.js';
 
-manageProjects.addProject(1, 'Default');
-manageProjects.addProject(2, 'Work');
-manageProjects.addProject(3, 'Personal');
+manageProjects.addProject(1, 'Project 1'); // Create Project 1
+const project1 = manageProjects.getProjects()[0]; // Get Project 1
+console.log(project1)
 
-const task1 = manageTasks.createTask(1, 'Task 1', 'Complete the report', '2025-01-15', true, true);
-const task2 = manageTasks.createTask(2, 'Task 2', 'Buy groceries', '2025-01-16', true, false);
-const task3 = manageTasks.createTask(3, 'Task 3', 'Fix the bug', '2025-01-17', true, false);
+const task1 = manageTasks.createTask(1, 'Task 1', 'Description of Task 1', '2025-02-20', true, false);
+const task2 = manageTasks.createTask(2, 'Task 2', 'Description of Task 2', '2025-02-21', true, true);
 
-manageProjects.getProjects()[1].addTask(task1); 
-manageProjects.getProjects()[2].addTask(task2); 
-manageProjects.getProjects()[1].addTask(task3); 
+project1.addTask(task1);
+project1.addTask(task2);
 
-console.log('Projects after adding tasks:', manageProjects.getProjects());
-
-manageProjects.getProjects()[1].deleteTask(1); 
-
-console.log('Projects after deleting a task:', manageProjects.getProjects());
-
+console.log('Tasks in Project 1 after adding tasks:', project1.tasks);
