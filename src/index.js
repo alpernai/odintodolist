e60efcfeac15bc6b65541b7d861fc59b2e5f58f7
Alejandro Perez Naitoh <alejandropereznaitoh@APN.local>
2025-03-24 13:11:08 -0600

    Project re-started

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-import './styles.css';  
-import manageProjects from './projects.js';
-import manageTasks from './tasks.js';
-import controlDOM from './controldom.js';
-
-controlDOM.displayAddProject();
