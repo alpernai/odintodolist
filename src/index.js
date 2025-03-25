4097a0a3e0776411d1cca7944cc81ff17da2450d
Alejandro Perez Naitoh <alejandropereznaitoh@APN.local>
2025-03-24 18:40:31 -0600

    HTML structure created.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { saveDataToLocalStorage, loadDataFromLocalStorage } from './storage';
-
-// Load projects from localStorage when the app starts
-const projects = loadDataFromLocalStorage();
-
-// Now you can work with the `projects` array
-console.log(projects);  // Check the console to ensure it's loading the data
